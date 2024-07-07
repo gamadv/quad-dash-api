4c5d8dc2135702c8e48e95711be397d2fd13b02b
Gamadv <lealdev.py@gmail.com>
2024-07-07 13:26:48 -0300

    ✨ feat(Env): Create env config plus validation - Change configs to env content

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'drizzle-kit'
+import { env } from './src/env'
 
 export default {
   schema: './src/db/schema/index.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgres://docker:docker@localhost:5432/quad_dash',
+    url: env.DATABASE_URL,
   },
 } satisfies Config
