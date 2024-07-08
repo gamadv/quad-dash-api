21751c5bed3bf2938051d249582b92c176d28385
Gamadv <lealdev.py@gmail.com>
2024-07-08 18:38:51 -0300

    ✨ feat(Server): Update server instances - Insert new endpoints (SendAuthLink / AuthenticateFromLink)

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,13 @@
 import { Elysia } from 'elysia'
 
+import { authenticateFromLink } from './routes/authenticate-from-link'
 import { registerCompany } from './routes/register-company'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const mainApp = new Elysia().use(registerCompany).use(sendAuthLink)
+const mainApp = new Elysia()
+  .use(registerCompany)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 mainApp.listen(3000, () => {
   console.info('🔥 HTTP server running!!')
