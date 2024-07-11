ae9f8d084f40998aba88175c89f9ec1fa4de0b3b
Gamadv <lealdev.py@gmail.com>
2024-07-11 18:43:07 -0300

    ✨ feat(route): Get profile started

---
+++ src/http/routes/get-profile.ts
@@ -0,0 +1,23 @@
+import Elysia from 'elysia'
+
+import { db } from '../../db/connection'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+
+export const getProfile = new Elysia()
+  .use(auth)
+  .get('/me', async ({ getCurrentUser }) => {
+    const { userId, companyId } = await getCurrentUser()
+
+    const user = await db.query.users.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.id, userId)
+      },
+    })
+
+    if (!user) {
+      throw new UnauthorizedError()
+    }
+
+    return user
+  })
