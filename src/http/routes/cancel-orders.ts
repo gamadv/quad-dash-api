0bd5566ceef1e5d348ccd10590fd3c6892109e76
Gamadv <lealdev.py@gmail.com>
2024-07-28 14:03:43 -0300

    ✨ feat(Service): Create route to cancel orders

---
+++ src/http/routes/cancel-orders.ts
@@ -0,0 +1,47 @@
+import { eq } from 'drizzle-orm'
+import Elysia, { t } from 'elysia'
+
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+
+export const cancelOrder = new Elysia().use(auth).patch(
+  '/orders/:orderId/cancel',
+  async ({ getCurrentUser, set, params }) => {
+    const { orderId } = params
+    const { companyId } = await getCurrentUser()
+
+    if (!companyId) {
+      throw new UnauthorizedError()
+    }
+
+    const order = await db.query.orders.findFirst({
+      where(fields, { eq, and }) {
+        return and(eq(fields.id, orderId), eq(fields.companyId, companyId))
+      },
+    })
+
+    if (!order) {
+      set.status = 400
+
+      return { message: 'Order not found.' }
+    }
+
+    if (!['pending', 'processing'].includes(order.status)) {
+      set.status = 400
+
+      return { message: 'You cannot cancel orders after dispatch.' }
+    }
+
+    await db
+      .update(orders)
+      .set({ status: 'canceled' })
+      .where(eq(orders.id, orderId))
+  },
+  {
+    params: t.Object({
+      orderId: t.String(),
+    }),
+  },
+)
