79dbf422f7021990b25310d09447e74c0caafeaf
Gamadv <lealdev.py@gmail.com>
2024-07-28 14:03:17 -0300

    ✨ feat(Service): Create route to approve orders

---
+++ src/http/routes/approve-orders.ts
@@ -0,0 +1,47 @@
+import { eq } from 'drizzle-orm'
+import Elysia, { t } from 'elysia'
+
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+
+export const approveOrder = new Elysia().use(auth).patch(
+  '/orders/:orderId/approve',
+  async ({ getCurrentUser, set, params }) => {
+    const { orderId } = params
+    const { companyId } = await getCurrentUser()
+
+    if (!companyId) {
+      throw new UnauthorizedError()
+    }
+
+    const order = await db.query.orders.findFirst({
+      where(fields, { eq, and }) {
+        return and(eq(fields.id, orderId), eq(fields.companyId, companyId))
+      },
+    })
+
+    if (!order) {
+      set.status = 400
+
+      return { message: 'Order not found.' }
+    }
+
+    if (order.status !== 'pending') {
+      set.status = 400
+
+      return { message: 'You can only approve pending orders.' }
+    }
+
+    await db
+      .update(orders)
+      .set({ status: 'processing' })
+      .where(eq(orders.id, orderId))
+  },
+  {
+    params: t.Object({
+      orderId: t.String(),
+    }),
+  },
+)
